097900951f5624075bdcb43a7cd5b08c0b18977d
wyyx <gnehcyx@163.com>
2019-06-03 15:22:45 +0800

    add check-password-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
@@ -7,8 +7,11 @@ const routes: Routes = [
     path: 'check-device',
     loadChildren: './pages/check-device/check-device.module#CheckDevicePageModule'
   },
-  { path: 'check-password', loadChildren: './pages/check-password/check-password.module#CheckPasswordPageModule' }
-];
+  {
+    path: 'check-password',
+    loadChildren: './pages/check-password/check-password.module#CheckPasswordPageModule'
+  }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
