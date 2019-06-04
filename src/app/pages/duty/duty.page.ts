5482f2394f98c939361b16946b946e80db7a95f8
wyyx <gnehcyx@163.com>
2019-06-04 15:12:29 +0800

    refactor select grade and class

--- src/app/pages/duty/duty.page.ts
+++ src/app/pages/duty/duty.page.ts
@@ -3,7 +3,7 @@ import { IonSelect } from '@ionic/angular'
 import { listAnim } from 'src/app/animations/list.anim'
 import { SelectScoreModalComponent } from 'src/app/components/select-score-modal/select-score-modal.component'
 import { deductionCategoryList, grades } from 'src/app/configs/class.config'
-import { AClass, DeductionCatetory } from 'src/app/models/duty.model'
+import { AClass, DeductionCatetory, Grade } from 'src/app/models/duty.model'
 import { PopoverService } from 'src/app/services/popover.service'
 import { ToastService } from 'src/app/services/toast.service'
 
@@ -18,17 +18,24 @@ const SIZE_PRE_SLIDE = 5
 export class DutyPage implements OnInit, AfterViewInit {
   @ViewChild('gradeSelect') gradeSelect: IonSelect
 
-  grade: number
+  gradeValue: number
   grades = grades
 
-  currentGradeName = '请选择班级'
-  currentClasses: AClass[] = []
+  currentGrade: Grade = {} as Grade
+  currentClass: AClass = {} as AClass
 
   deductionCatogoryList: DeductionCatetory[] = []
 
   slideOpts = {
     initialSlide: 0,
-    speed: 400
+    speed: 400,
+    autoHeight: true
+  }
+
+  get currentClassFullName() {
+    return this.currentGrade.label && this.currentClass.label
+      ? this.currentGrade.label + this.currentClass.label
+      : '未选班级'
   }
 
   constructor(private popoverService: PopoverService, private toastService: ToastService) {}
@@ -36,19 +43,34 @@ export class DutyPage implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {}
 
   openSelectGrade(event) {
+    console.log('TCL: DutyPage -> openSelectGrade -> event', event)
     this.gradeSelect.open(event)
   }
 
   onGradeChange(event) {
-    const grade = this.grades.filter(e => e.value === this.grade)[0]
+    const grade = this.grades.filter(e => e.value === this.gradeValue)[0]
     if (grade) {
-      this.currentGradeName = grade.label
-      this.currentClasses = grade.classes
+      this.currentGrade = grade
     }
   }
 
   ngOnInit() {
     this.loadDeductionCategory()
+    this.loadGrades()
+    this.setFirstGrade()
+    this.setFirstClass()
+  }
+
+  setFirstGrade() {
+    const firstGrade = this.grades[0]
+
+    if (firstGrade) {
+      this.currentGrade = firstGrade
+    }
+  }
+
+  setFirstClass() {
+    this.currentClass = this.currentGrade.classes[0]
   }
 
   openSelectScorePopover() {
@@ -75,16 +97,11 @@ export class DutyPage implements OnInit, AfterViewInit {
     this.deductionCatogoryList = deductionCategoryList
   }
 
-  // setSlides(deductionsOptions: DeducionOption[]) {
-  //   const groups = Math.ceil(this.deductionOptions.length / SIZE_PRE_SLIDE)
+  loadGrades() {
+    this.grades = grades
+  }
 
-  //   for (let index = 0; index < groups; index++) {
-  //     const slide = this.deductionOptions.slice(
-  //       index * SIZE_PRE_SLIDE,
-  //       index * SIZE_PRE_SLIDE + SIZE_PRE_SLIDE
-  //     )
-
-  //     this.slides.push(slide)
-  //   }
-  // }
+  setClass(aclass: AClass) {
+    this.currentClass = aclass
+  }
 }
